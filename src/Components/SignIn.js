522c2a70fce6eb61bfb99a4f4181c72c884e0ea2
Abhi9125 <abhi.singh3132@gmail.com>
2024-03-20 15:16:41 +0530

    part - 2 update.

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -1,15 +1,23 @@
+// Importing React and useState hook from React library
 import React, { useState } from "react";
+// Importing the Header component
 import Header from "./Header";
 
+// SignIn component definition
 const SignIn = () => {
+  // State hook for managing sign-in and sign-up toggle
   const [isSignIn, setIsSignIn] = useState(true);
 
+  // Event handler for toggling between sign-in and sign-up
   const handleSignInAndSignUp = (e) => {
-    e.preventDefault();
-    return setIsSignIn(!isSignIn);
+    e.preventDefault(); // Event handler for toggling between sign-in and sign-up
+    return setIsSignIn(!isSignIn); // Toggles the state between true and false
   };
+
+  // Component return statement
   return (
     <div>
+      {/* Renders the Header component */}
       <Header />
       <div className="absolute">
         <img
@@ -18,32 +26,43 @@ const SignIn = () => {
         />
       </div>
 
+      {/* Form for sign-in or sign-up */}
       <form className="w-3/12 absolute p-12 bg-black my-28 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
-        <h1 className="font-bold text-2xl">
+        {/* Dynamic heading based on sign-in or sign-up state */}
+        <h1 className="font-bold text-2xl my-4">
           {isSignIn ? "Sign in" : "Sign up"}
         </h1>
+        {/* Conditional rendering for sign-up input field */}
+        {isSignIn === false && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-3 my-3 w-full bg-gray-700 rounded-lg"
+          />
+        )}
+        {/* Input fields for email/phone and password */}
         <input
           type="text"
           placeholder="Enter email/phone"
-          className="p-4 my-4 w-full bg-gray-700"
+          className="p-3 my-3 w-full bg-gray-700 rounded-lg"
         />
         <input
           type="text"
           placeholder="Password"
-          className="p-4 my-4 w-full bg-gray-700"
+          className="p-3 my-3 w-full bg-gray-700 rounded-lg"
         />
-        <button className="p-4 my-4 w-full bg-red-600 rounded-lg">
+        <button className="p-3 my-3 w-full bg-red-600 rounded-lg">
           {isSignIn ? "Sign in" : "Sign up"}
         </button>
         {isSignIn && (
           <>
             <p className="text-center">OR</p>
-            <button className="p-4 my-4 w-full rounded-lg bg-gray-300 bg-opacity-20">
+            <button className="p-3 my-3 w-full rounded-lg bg-gray-300 bg-opacity-20">
               Use a sign-in code
             </button>
           </>
         )}
-
+        {/* Toggle link between sign-in and sign-up */}
         <div>
           {isSignIn ? "New to Netflix? " : "Already a user? "}
 
